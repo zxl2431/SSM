9b23ccb0afe7e2805fd77447e81a02f297544b05
zhangxianlin <zhangxianlin2431@163.com>
2022-04-05 00:30:42 +0800

    早期JDBC操作,注册驱动时用register方法 只要有mysql-connector-java.jar包 就行

--- spring-demo01/src/main/java/cn/agree/jdbc/Demo.java
+++ spring-demo01/src/main/java/cn/agree/jdbc/Demo.java
@@ -1,11 +1,31 @@
 package cn.agree.jdbc;
 
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Demo {
-    public static void main(String[] args) throws ClassNotFoundException {
-        // 注册驱动
-        Class.forName("driver");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1 注册驱动
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        // Class<?> driver = Class.forName("driver");
+
+        //2 获取连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/db1", "root", "root");
+        System.out.println(connection);
+
+        //3 获取操作数据库的预处理对象
+        PreparedStatement pstm = connection.prepareStatement("select * from account");
+        //4 执行SQL
+        ResultSet resultSet = pstm.executeQuery();
+        //5 遍历结果
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+        //6 关闭资源
+        resultSet.close();
+        pstm.close();
+        connection.close();
+
+
 
     }
 }
