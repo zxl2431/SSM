97cec3962cacc03e72805659c747f088ee727538
zhangxianlin <zhangxianlin2431@163.com>
2022-04-05 18:22:46 +0800

    spring中ApplicationContext和BeanFactory的区别,生命周期、单利和多利模式

--- spring-demo01/src/main/java/cn/agree/service/impl/AccountServiceImpl.java
+++ spring-demo01/src/main/java/cn/agree/service/impl/AccountServiceImpl.java
@@ -8,15 +8,35 @@ import cn.agree.service.AccountService;
 public class AccountServiceImpl implements AccountService {
 
     // private AccountDao accountDao = new AccountDaoImpl();
-    private  AccountDao accountDao = (AccountDao) BeanFactory.getBean("accountDao");
+    // private  AccountDao accountDao = (AccountDao) BeanFactory.getBean("accountDao");
+
+    /*
+    *  无参构造方法
+    * */
+    public AccountServiceImpl() {
+        System.out.println("创建AccountServiceImpl...");
+    }
+
+    /*
+    *  bean的作用和生命周期
+    *  init()
+    *  destory()
+    * */
+    public void init() {
+        System.out.println("AccountServiceImpl初始化了...");
+    }
+
+    public void destory() {
+        System.out.println("AccountServiceImpl销毁了...");
+    }
 
 
     @Override
     public void saveAccount() {
 
-        System.out.println("service层保存账户..."+accountDao);
+        System.out.println("service层保存账户...");
         // BeanFactory.printBean();
-        accountDao.saveAccount();
+        // accountDao.saveAccount();
     }
 
 }
--- spring-demo01/src/test/java/SpringIOCTest01.java
+++ spring-demo01/src/test/java/SpringIOCTest01.java
@@ -1,6 +1,11 @@
 import cn.agree.dao.AccountDao;
 import cn.agree.service.AccountService;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class SpringIOCTest01 {
     public static void main(String[] args) {
@@ -14,6 +19,33 @@ public class SpringIOCTest01 {
 
         accountService.saveAccount();
         // accountDao.saveAccount();
+
+
+    }
+
+    /*
+    *  ApplicationContext: 只要一读取配置文件,默认情况下就会创建对象
+    *  BeanFactory: 什么时候使用什么时候创建
+    *
+    * */
+    @Test
+    public void testApplicationContext() {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        int count = ac.getBeanDefinitionCount();
+        System.out.println(count);
+
+    }
+
+    @Test
+    public void testBeanFactory() {
+        // 读取resource
+        Resource resource = new ClassPathResource("bean.xml");
+
+        // 创建BeanFactory对象
+        BeanFactory beanFactory = new XmlBeanFactory(resource);
+
+        // 获取实例 此时才会创建bean
+        AccountService accountService = (AccountService) beanFactory.getBean("accountService");
 
 
     }
