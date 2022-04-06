d7ed35f15bb2caa44ac2d85bdf0f66f6a239bb9e
zhangxianlin <zhangxianlin2431@163.com>
2022-04-06 11:22:53 +0800

    之前JdbcConfig里面数据源的参数都是写死在class中的,通过 @PropertySource注解可以写到配置文件中 @Value注解可以读取配置文件中的值到class的属性中

--- spring-annotation/src/main/java/cn/agree/config/JdbcConfig.java
+++ spring-annotation/src/main/java/cn/agree/config/JdbcConfig.java
@@ -2,13 +2,25 @@ package cn.agree.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
 
+@PropertySource("classpath:jdbc.properties")
 public class JdbcConfig {
+
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
 
     /*
     * 创建一个数据源,并存入spring容器中
@@ -19,10 +31,18 @@ public class JdbcConfig {
     public DataSource createDataSource() {
         try {
             ComboPooledDataSource ds = new ComboPooledDataSource();
-            ds.setUser("root");
-            ds.setPassword("root");
-            ds.setDriverClass("com.mysql.jdbc.Driver");
-            ds.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/db1");
+            /* 这是写死的模式 并不推荐 */
+            // ds.setUser("root");
+            // ds.setPassword("root");
+            // ds.setDriverClass("com.mysql.jdbc.Driver");
+            // ds.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/db1");
+
+            /*使用配置文件的方式*/
+            ds.setUser(username);
+            ds.setPassword(password);
+            ds.setDriverClass(driver);
+            ds.setJdbcUrl(url);
+
             return ds;
         } catch (PropertyVetoException e) {
             throw new RuntimeException(e);
