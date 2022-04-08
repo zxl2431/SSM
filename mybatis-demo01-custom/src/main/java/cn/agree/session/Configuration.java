865696c4346ae9622f291e069f87477467ccfeb3
zhangxianlin <zhangxianlin2431@163.com>
2022-04-08 08:14:58 +0800

    configuration有了设置数据源的基本信息之后,需要有获取连接的功能

--- mybatis-demo01-custom/src/main/java/cn/agree/session/Configuration.java
+++ mybatis-demo01-custom/src/main/java/cn/agree/session/Configuration.java
@@ -1,8 +1,15 @@
 package cn.agree.session;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /*
 *  Configuration对象
-*  存储解析数据库连接信息
+*  1、存储解析数据库连接信息
+*  2、具备获取数据库连接Connection的功能
 *
 * */
 public class Configuration {
@@ -14,6 +21,14 @@ public class Configuration {
     private String url;
     // 数据库驱动
     private String driver;
+
+    // 创建数据源
+    private ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+
+    public void setDataSource(ComboPooledDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public String getUsername() {
         return username;
@@ -45,6 +60,35 @@ public class Configuration {
 
     public void setDriver(String driver) {
         this.driver = driver;
+    }
+
+    /*
+     * 创建数据库连接池
+     *
+     * */
+    public ComboPooledDataSource getDataSource() {
+        try {
+            dataSource.setPassword(this.password);
+            dataSource.setUser(this.username);
+            dataSource.setJdbcUrl(this.url);
+            dataSource.setDriverClass(this.driver);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    /*
+    *  获取数据库连接
+    *
+    * */
+    public Connection getConnection() {
+        try {
+            return getDataSource().getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
