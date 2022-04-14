7d9460f4c2cf07fbd757a546ffa72e69efb84551
zhangxianlin <zhangxianlin2431@163.com>
2022-04-14 23:10:13 +0800

    昨天返回404是因为springmvc的注解扫描没开 在springmvc的controller中实现基于ServletAPI的返回,不经过视图解析层

--- springmvc-demo02/src/main/java/cn/agree/controller/UserController.java
+++ springmvc-demo02/src/main/java/cn/agree/controller/UserController.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 
 @Controller
@@ -21,9 +26,33 @@ public class UserController {
         user.setMoney(100d);
         user.setBirthday(new Date());
         model.addAttribute("user", user);
-        return "success";
+        return "updateUser";
 
     }
+
+    /*
+    *  向用户输出一段话,不跳转到某个jsp
+    *
+    *  使用ServletAPI
+    *   直接将对应的ServletAPI中的对象当中参数传入即可使用
+    *
+    * */
+    @RequestMapping(value = "/add/text")
+    public void addUesrWriterText(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
+        System.out.println("void");
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+
+        // 获取一个PrintWriter对象
+        PrintWriter writer = response.getWriter();
+        writer.write("你说啥就是傻");
+        writer.flush();
+        writer.close();
+        return;
+    }
+
+
 
 
 }
